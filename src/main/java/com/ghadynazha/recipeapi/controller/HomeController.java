ed8718884d7fe3a35676f396b7c2fc77162c4600
GHADY NAZHA <ghadynazha25@gmail.com>
2025-05-18 22:21:50 +0300

    Add HomeController to expose / for Render

---
+++ src/main/java/com/ghadynazha/recipeapi/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.ghadynazha.recipeapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "✅ Welcome to the Recipe API!";
+    }
+}
+
